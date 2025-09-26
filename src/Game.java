b76be8eba52508446b5f73d4942d208a0618b654
yuanyangCyy <YC43@myscc.ca>
2025-09-25 22:01:20 -0400

    done lab 6

--- src/Game.java
+++ src/Game.java
@@ -5,39 +5,57 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the scissor game!");
-        System.out.println("Scissor (0), rock(1), paper(2), lizard (3), spock (4) : ");
-        int user = scanner.nextInt(); //set user input 0/1/2 add 3 4
+        Random randome = new Random();
 
-
-        int computer = new Random().nextInt(5); //0-2
-
-        //change to the text
 
         String[] names = {"scissor", "rock", "paper", "lizard", "spock"};
-        String userChoice = names[user];
-        String computerChoice = names[computer];
 
 
-        //return the result
+        int userScore = 0;
+        int computerScore = 0;
 
-        String result;
-        if (user == computer) {
-            result = "It is a draw";
-        } else if ((user == 0 &&  (computer == 2 || computer == 3))
-        || (user == 1 && ( computer == 0 || computer == 3 ))
-        || (user == 2 && (computer == 1 || computer == 4))
-        || (user == 3 && (computer == 2 || computer == 4))
-        || (user == 4 && (computer == 0 || computer == 1)))
 
-        {
-            result = "You won";
 
-        } else {
-            result = "You lost";
+        while (true) {
+            System.out.println("Scissor (0), rock(1), paper(2), lizard (3), spock (4), exit(8) :");
+            int user = scanner.nextInt();
+            if (user == 8) break;
+            if (user < 0 || user > 4) {
+                System.out.println("Invalid user choice. Try again.");
+                continue;
+
+            }
+
+            int computer = randome.nextInt(5);
+
+            boolean userWin =
+                    (user == 0 && (computer == 2 || computer == 3))
+                            || (user == 1 && (computer == 0 || computer == 3))
+                            || (user == 2 && (computer == 1 || computer == 4))
+                            || (user == 3 && (computer == 2 || computer == 4))
+                            || (user == 4 && (computer == 0 || computer == 1));
+
+            String result;
+            if (user == computer) {
+                result = "It is a draw";
+            } else if (userWin) {
+                result = "You win";
+                userScore++;
+            } else {
+                result = "You lose";
+                computerScore++;
+            }
+
+
+            System.out.println("The computer is " + names[computer] +
+                    ".You are" + names[user] + ". " + result);
+
+            System.out.println("Youre Score: " + userScore + "| Computer Score: " + computerScore);
+
         }
 
+System.out.println("Final Score: " + userScore + "| Computer Score: " + computerScore);
+        System.out.println("over");
 
-        System.out.println("The computer is " + computerChoice + " you are " + userChoice + "."+ result);
     }
 }
