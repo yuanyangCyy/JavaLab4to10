9bbcff04e0b5523c89a27a510727ff72c6c21531
yuanyangCyy <YC43@myscc.ca>
2025-09-25 21:24:19 -0400

    Lab 5 done

--- src/Game.java
+++ src/Game.java
@@ -6,15 +6,15 @@ public class Game {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the scissor game!");
-        System.out.println("Scissor (0), rock(1), paper(2): ");
-        int user = scanner.nextInt(); //set user input 0/1/2
+        System.out.println("Scissor (0), rock(1), paper(2), lizard (3), spock (4) : ");
+        int user = scanner.nextInt(); //set user input 0/1/2 add 3 4
 
 
-        int computer = new Random().nextInt(3); //0-2
+        int computer = new Random().nextInt(5); //0-2
 
         //change to the text
 
-        String[] names = {"scissor", "rock", "paper"};
+        String[] names = {"scissor", "rock", "paper", "lizard", "spock"};
         String userChoice = names[user];
         String computerChoice = names[computer];
 
@@ -24,7 +24,13 @@ public class Game {
         String result;
         if (user == computer) {
             result = "It is a draw";
-        } else if ((user == 0 && computer == 2) || (user == 1 && computer == 0) || (user == 2 && computer == 1)) {
+        } else if ((user == 0 &&  (computer == 2 || computer == 3))
+        || (user == 1 && ( computer == 0 || computer == 3 ))
+        || (user == 2 && (computer == 1 || computer == 4))
+        || (user == 3 && (computer == 2 || computer == 4))
+        || (user == 4 && (computer == 0 || computer == 1)))
+
+        {
             result = "You won";
 
         } else {
@@ -32,6 +38,6 @@ public class Game {
         }
 
 
-        System.out.println("The computer" + computerChoice + " you are " + userChoice + "."+ result);
+        System.out.println("The computer is " + computerChoice + " you are " + userChoice + "."+ result);
     }
 }
